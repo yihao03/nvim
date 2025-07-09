8ff7ea7e60c46ff15ebe4d0fe652742ef32aea02
yihao <yihao039@gmail.com>
2025-07-09 20:29:05 +0800

    removed default configs and added animation configsq

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,21 +4,6 @@ return {
   build = ":Copilot auth",
   event = "BufReadPost",
   opts = {
-    suggestion = {
-      enabled = not vim.g.ai_cmp,
-      auto_trigger = true,
-      hide_during_completion = vim.g.ai_cmp,
-      keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
-        next = "<M-]>",
-        prev = "<M-[>",
-      },
-    },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
-    },
     copilot_node_command = "/home/yihao/.nodenv/versions/24.1.0/bin/node",
   },
 }
---
+++ lua/plugins/cursor.lua
@@ -0,0 +1,20 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  event = "VeryLazy",
+  cond = vim.g.neovide == nil,
+  opts = {
+    hide_target_hack = true,
+    cursor_color = "none",
+    smear_between_buffers = false,
+  },
+  specs = {
+    -- disable mini.animate cursor
+    {
+      "echasnovski/mini.animate",
+      optional = true,
+      opts = {
+        cursor = { enable = false },
+      },
+    },
+  },
+}
