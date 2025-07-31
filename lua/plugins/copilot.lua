c426b2f05da2378ca21b26b0b4021bf0afc0b519
yihao <yihao039@gmail.com>
2025-07-31 17:21:56 +0800

    prepare for copilotchat v4

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,9 +1,30 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  event = "BufReadPost",
-  opts = {
-    copilot_node_command = "/home/yihao/.nodenv/versions/24.1.0/bin/node",
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      copilot_node_command = "/home/yihao/.nvm/versions/node/v24.4.1/bin/node",
+    },
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    version = "3.12.2",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken",
+    opts = {
+      model = "o4-mini",
+      headers = {
+        user = " ",
+        assistant = "  Copilot",
+      },
+      window = {
+        width = 0.3,
+      },
+    },
+    -- See Commands section for default commands if you want to lazy load on them
   },
 }
