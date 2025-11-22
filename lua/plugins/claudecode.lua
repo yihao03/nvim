f863f432332a2a1dd64b85f810424c7bf9c48f1a
yihao <yihao039@gmail.com>
2025-11-22 18:19:54 +0800

    disable default keymaps

--- lua/plugins/claudecode.lua
+++ lua/plugins/claudecode.lua
@@ -3,6 +3,18 @@ return {
     "coder/claudecode.nvim",
     opts = {},
     keys = {
+      -- disable default keymaps
+      { "<leader>a", false },
+      { "<leader>ac", false },
+      { "<leader>af", false },
+      { "<leader>ar", false },
+      { "<leader>aC", false },
+      { "<leader>ab", false },
+      { "<leader>as", false },
+      { "<leader>as", false },
+      -- Diff management
+      { "<leader>aa", false },
+      { "<leader>ad", false },
       -- Use <leader>h for Claude (easy to type and remember)
       { "<leader>h", "", desc = "+claude", mode = { "n", "v" } },
       { "<leader>hh", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
