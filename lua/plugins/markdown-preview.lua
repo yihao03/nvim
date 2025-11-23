38d003428a6ebcad181cc19f9b22025f2b0d9abe
yihao <yihao039@gmail.com>
2025-11-23 21:51:49 +0800

    Add print format toggle

---
+++ lua/plugins/markdown-preview.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    keys = {
+      {
+        "<leader>mc",
+        function()
+          if vim.bo.filetype ~= "markdown" then
+            print("Option only available in markdown files")
+            return
+          end
+
+          if vim.g.mkdp_markdown_css:match("cheatsheet") then
+            vim.g.mkdp_markdown_css = vim.fn.stdpath("config") .. "/styles/standard.css"
+            print("Switched to standard.css")
+          else
+            vim.g.mkdp_markdown_css = vim.fn.stdpath("config") .. "/styles/cheatsheet.css"
+            print("Switched to cheatsheet.css")
+          end
+          -- Restart preview to apply new CSS
+          vim.cmd("MarkdownPreviewStop")
+          vim.cmd("MarkdownPreview")
+        end,
+        desc = "Toggle Markdown CSS Style",
+      },
+    },
+  },
+}
