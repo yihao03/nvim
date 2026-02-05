454d11bf987a11708aa30344c34fcd82254190bb
yihao <yihao039@gmail.com>
2026-02-05 12:42:26 +0800

    Add gh info to dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,21 +4,54 @@ return {
     dashboard = {
       preset = {
         header = [[
-░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
- ░▒▓█▓▒▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
- ░▒▓█▓▒▒▓█▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░   
-  ░▒▓█▓▓█▓▒░        ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
-  ░▒▓█▓▓█▓▒░        ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
-   ░▒▓██▓▒░  ░▒▓███████▓▒░ ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░ 
+ █████   █████  █████████    █████████               █████         
+▒▒███   ▒▒███  ███▒▒▒▒▒███  ███▒▒▒▒▒███             ▒▒███          
+ ▒███    ▒███ ▒███    ▒▒▒  ███     ▒▒▒   ██████   ███████   ██████ 
+ ▒███    ▒███ ▒▒█████████ ▒███          ███▒▒███ ███▒▒███  ███▒▒███
+ ▒▒███   ███   ▒▒▒▒▒▒▒▒███▒███         ▒███ ▒███▒███ ▒███ ▒███████ 
+  ▒▒▒█████▒    ███    ▒███▒▒███     ███▒███ ▒███▒███ ▒███ ▒███▒▒▒  
+    ▒▒███     ▒▒█████████  ▒▒█████████ ▒▒██████ ▒▒████████▒▒██████ 
+     ▒▒▒       ▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒   ▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒  
 ]],
       },
       sections = {
         { section = "header" },
-        -- { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = { 2, 2 } },
-        { section = "recent_files", indent = 2, padding = { 2, 2 } },
-        -- { section = "keys", gap = 1, padding = 1 },
-        -- { section = "startup" },
+        { pane = 1, section = "recent_files" },
+        function()
+          local in_git = Snacks.git.get_root() ~= nil
+          local cmds = {
+            {
+              title = "Open Issues",
+              cmd = "gh issue list -L 3",
+              key = "i",
+              action = function()
+                vim.fn.jobstart("gh issue list --web", { detach = true })
+              end,
+              icon = " ",
+              height = 7,
+            },
+            {
+              icon = " ",
+              title = "Open PRs",
+              cmd = "gh pr list -L 3",
+              key = "P",
+              action = function()
+                vim.fn.jobstart("gh pr list --web", { detach = true })
+              end,
+              height = 7,
+            },
+          }
+          return vim.tbl_map(function(cmd)
+            return vim.tbl_extend("force", {
+              pane = 2,
+              section = "terminal",
+              enabled = in_git,
+              padding = 1,
+              ttl = 5 * 60,
+              indent = 3,
+            }, cmd)
+          end, cmds)
+        end,
       },
     },
     picker = {
