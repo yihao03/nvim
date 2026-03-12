1cfac9560e9733b21a19e20a4fbf2a6ef18ae7a0
yihao <yihao039@gmail.com>
2026-03-12 17:10:37 +0800

    Add git blame toggle shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,4 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>gl", "<cmd>Gitsigns toggle_current_line_blame<cr>", { desc = "Toggle Git Blame" })
