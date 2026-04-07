abe5eca5da304152cb394cafd160cd1d282803fa
yihao <yihao039@gmail.com>
2026-04-07 15:52:35 +0800

    Enable and disable copilot

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>gl", "<cmd>Gitsigns toggle_current_line_blame<cr>", { desc = "Toggle Git Blame" })
+
+vim.api.nvim_create_user_command("DisableCopilot", "lsp stop copilot", {})
+vim.api.nvim_create_user_command("EnableCopilot", "lsp enable copilot", {})
