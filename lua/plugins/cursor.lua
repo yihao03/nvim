fb0d795b679d8c8fa3207eed3f1e7808713bcdac
yihao <yihao039@gmail.com>
2026-04-18 14:35:46 +0800

    cursor configs

--- lua/plugins/cursor.lua
+++ lua/plugins/cursor.lua
@@ -1,17 +1,14 @@
 return {
   "sphamba/smear-cursor.nvim",
   event = "VeryLazy",
-  cond = vim.g.neovide == nil,
   opts = {
     smear_between_buffers = false,
-  },
-  specs = {
-    {
-      "nvim-mini/mini.animate",
-      optional = true,
-      opts = {
-        cursor = { enable = false },
-      },
-    },
+    smear_insert_mode = false,
+    smear_to_cmd = false,
+
+    -- appearance
+    stiffness = 0.8,
+    trailing_stiffness = 0.3,
+    anticipation = 0,
   },
 }
